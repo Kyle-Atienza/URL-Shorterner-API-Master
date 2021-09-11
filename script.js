c638f0818ad711e3d18993df0cbe4052dddce26e
Kyle Atienza <atienzakylefrancis@gmail.com>
2021-09-11 14:34:56 +0800

    Working on API

--- script.js
+++ script.js
@@ -3,6 +3,27 @@ const toggler = document.getElementById("nav-toggler")
 const nav = document.getElementById("nav-header")
 
 toggler.addEventListener('click', () => {
-    // nav.classList.toggle("hide")
-    nav.style.display = nav.style.display === 'none' ? '' : 'none'
+    nav.classList.toggle("show")
+    // var navDisplay = nav.style.display === 'grid' ? 'none' : 'grid'
+    // nav.style.display = navDisplay;
+
+
+})
+
+
+
+var longLink = document.getElementById("linkInput")
+var submitLink = document.getElementById("submitLink")
+
+submitLink.addEventListener('click', () => {
+    var link = longLink.val()
+    console.log(link)
+    $.ajax({
+        url: "https://api.shrtco.de/v2/shorten?url=" + link + ".org/very/long/link.html"
+    },
+        function () {
+            console.log(result.short_link)
+        }
+    )
+
 })
